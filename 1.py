a1a572a82af84c7ea282a144aa50c92e6eeddcc0
tjsl1124 <tjsl1124@korea.ac.kr>
2019-09-18 17:20:56 +0900

    1번파일 커밋

---
+++ 1.py
@@ -0,0 +1,12 @@
+import time
+
+def test():
+    sum =0
+    for i in range (1,1000):
+        if i%3 ==0 or i %5 ==0: sum+=i
+    print('%d' %(sum))
+
+if __name__ == "__main__":
+    start = time.time()
+    test()
+    print("time :", time.time() - start)
